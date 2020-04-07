4593261d7da41727d7d63c5180d215ed74815c4b
antoine-brun <antoine.brun@gmail.com>
2020-04-07 16:01:15 +0200

    add call to associate-address

--- Workflows/Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/Task_Associate_Elastic_IP.php
+++ Workflows/Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/Task_Associate_Elastic_IP.php
@@ -14,6 +14,23 @@ $elastic_ip_id = $context["elastic_ip_id"];
 logToFile("instance ID: ".$instance_id);
 logToFile("elastic IP ID: ".$elastic_ip_id);
 
+$ec2Client = Ec2Client::factory(array(
+    'key'    => $context["key"],
+    'secret' => $context["secret"],
+    'region' => $context["region"]
+));
+
+logToFile("ec2 client successful");
+logToFile(debug_dump($context, "DEBUG CONTEXT:\n"));
+
+$result = $ec2Client->associate-address($elastic_ip_id, $instance_id);
+try {
+    $res = $result->toArray();
+    logToFile(debug_dump($res, "AWS response\n"));
+}
+catch (Exception $e) {
+    task_exit(FAILED, "Error : $e");
+}
 #$context["device_ip_address"] = $elastic_ip;
 
 task_success('Task OK: Elastic IP".$elastic_ip_id." associated');
