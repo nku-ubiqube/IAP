7ad175ebf31947d7bb0ba9341609c9df3ede7ef6
abr-ubiqube <antoine.brun@ubiqube.com>
2020-04-08 21:45:40 +0000

    add additionnal conn check

--- Workflows/Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/Task_Wait_for_device_Ping.php
+++ Workflows/Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/Task_Wait_for_device_Ping.php
@@ -11,17 +11,49 @@ require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
 function list_args()
 {
 }
-/*****
+
+
+
+$PROCESSINSTANCEID = $context['PROCESSINSTANCEID'];
+$EXECNUMBER = $context['EXECNUMBER'];
+$TASKID = $context['TASKID'];
+$process_params = array('PROCESSINSTANCEID' => $PROCESSINSTANCEID,
+						'EXECNUMBER' => $EXECNUMBER,
+						'TASKID' => $TASKID);
+
+
 $a = ".";
-for ($index = 0; $index < 5; $index++) {
+for ($index = 0; $index < 6; $index++) {
     $a .= ".";
     update_asynchronous_task_details($context, "Wait for device Ping" . $a . "\nloop no : " . ($index+1));
-    sleep(60);
+    sleep(10);
 }
 
-task_exit(ENDED, "Task OK");
-****/
 
-task_exit(ENDED, "skipping task");
+$device_ip_address = $context["device_ip_address"];
+
+/* test Ping */
+$response = wait_for_ping_status($device_ip_address, $process_params);
+$response = json_decode($response, true);
+if ($response['wo_status'] !== ENDED) {
+	$response = json_encode($response);
+	echo $response;
+	exit;
+}
+$ping_status_message = $response['wo_comment'];
+
+/* test SSH */
+$port_no = SSH_DEFAULT_PORT_NO;
+$response = wait_for_ssh_status($device_ip_address, $port_no, $process_params);
+$response = json_decode($response, true);
+if ($response['wo_status'] !== ENDED) {
+	$response = json_encode($response);
+	echo $response;
+	exit;
+}
+
+
+task_exit(ENDED, "Task OK");
+
 
 ?>
